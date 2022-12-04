9f60f1f0803ca97126a3a97dbd36fbf7c6e0fda5
Sheri Bigelow <sheri@designsimply.com>
2022-12-04 12:23:10 -0800

    add helper bash scripts to run the application

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!bash
+
+# set up the right python
+source .venv/bin/activate
+
+# set up the environment variables                                                                                                                                                           ~/www/tinybasilempire
+source .secrets
+
+# run your application
+flask run
