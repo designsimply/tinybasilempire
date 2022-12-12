08be59e82ce0cc552fe090f331eecd7e24ee8704
Sheri Bigelow <sheri@designsimply.com>
2022-12-12 07:07:32 -0800

    Add j/k keyboard shortcuts to navigate through list items.

--- static/js/keyboard-shortcuts.js
+++ static/js/keyboard-shortcuts.js
@@ -14,16 +14,18 @@ function search() {
 // });
 
 // single keys
+i = 0;
 Mousetrap.bind('4', function () { console.log('4'); });
 Mousetrap.bind('/', function () { document.getElementById("q").focus(); return false; });
 Mousetrap.bind("?", function () { alert('show shortcuts!'); });
-Mousetrap.bind("h", function () { document.getElementById("home").click(); });
-Mousetrap.bind("t", function () { document.getElementById("tags").click(); });
-Mousetrap.bind("a", function () { document.getElementById("add").click(); });
-Mousetrap.bind("]", function () { document.getElementById("next").click(); });
-Mousetrap.bind("[", function () { document.getElementById("previous").click(); });
-Mousetrap.bind("j", function () { document.getElementById("next").click(); });
-Mousetrap.bind("k", function () { document.getElementById("previous").click(); });
+Mousetrap.bind("h", function () { document.getElementById('home').click(); });
+Mousetrap.bind("t", function () { document.getElementById('tags').click(); });
+Mousetrap.bind("a", function () { document.getElementById('add').click(); });
+Mousetrap.bind(['n', '['], function () { document.getElementById('next').click(); });
+Mousetrap.bind(['p', ']'], function () { document.getElementById('previous').click(); });
+Mousetrap.bind("o", function () { document.getElementsByClassName('current')[0].getElementsByClassName('link')[0].click(); });
+Mousetrap.bind("j", function () { cur = document.getElementsByClassName('item'); if (i > 0) { prev = i - 1; cur[prev].classList.remove('current'); }; if (cur[i]) { cur[i].classList.add('current'); i++; } else { i = 0; } });
+Mousetrap.bind("k", function () { cur = document.getElementsByClassName('item'); if (i >= 0) { i--; next = i + 1; cur[next].classList.remove('current'); }; if (cur[i]) { cur[i].classList.add('current'); } else { i = 0; } });
 Mousetrap.bind("s h e r i", function () { alert('hello sheri'); });
 //Mousetrap.bind('esc', function () { console.log('escape'); }, 'keyup');
 
