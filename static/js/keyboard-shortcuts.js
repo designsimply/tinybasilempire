415d44e8f3afca6f91a6a0f483e0cbb15ab307a4
Sheri Bigelow <sheri@designsimply.com>
2022-12-13 06:58:18 -0800

    Clean up keyboard shortcuts.

--- static/js/keyboard-shortcuts.js
+++ static/js/keyboard-shortcuts.js
@@ -4,49 +4,53 @@ function search() {
   search.focus();
 }
 
-// Mousetrap.bind({
-//   '/': document.getElementById("search").focus();,
-//   't': tweet,
-//   '?': function modal() { $('#help').modal('show'); },
-//   'j': function next() { highLight('j') },
-//   'k': function prev() { highLight('k') },
-//   'command+k': function () { alert("command+k"); return false; }
-// });
-
 // single keys
-i = 0;
-Mousetrap.bind('4', function () { console.log('4'); });
-Mousetrap.bind('/', function () { document.getElementById("q").focus(); return false; });
-Mousetrap.bind("?", function () { alert('show shortcuts!'); });
+Mousetrap.bind('/', function () { document.getElementById("q").focus(); document.getElementById("q").select(); return false; });
+Mousetrap.bind("?", function () { alert('TODO: show Keyboard Shortcuts Overlay'); });
 Mousetrap.bind("h", function () { document.getElementById('home').click(); });
 Mousetrap.bind("t", function () { document.getElementById('tags').click(); });
 Mousetrap.bind("a", function () { document.getElementById('add').click(); });
-Mousetrap.bind(['n', '['], function () { document.getElementById('next').click(); });
-Mousetrap.bind(['p', ']'], function () { document.getElementById('previous').click(); });
 Mousetrap.bind("o", function () { document.getElementsByClassName('current')[0].getElementsByClassName('link')[0].click(); });
-Mousetrap.bind("j", function () { cur = document.getElementsByClassName('item'); if (i > 0) { prev = i - 1; cur[prev].classList.remove('current'); }; if (cur[i]) { cur[i].classList.add('current'); i++; } else { i = 0; } });
-Mousetrap.bind("k", function () { cur = document.getElementsByClassName('item'); if (i >= 0) { i--; next = i + 1; cur[next].classList.remove('current'); }; if (cur[i]) { cur[i].classList.add('current'); } else { i = 0; } });
+Mousetrap.bind("e", function () { document.getElementsByClassName('current')[0].getElementsByClassName('edit')[0].click(); });
+Mousetrap.bind('#', function () { alert('# TODO: trigger delete for the active list item'); });
 Mousetrap.bind("s h e r i", function () { alert('hello sheri'); });
+Mousetrap.bind("d y l a n", function () { alert('hello dylan'); });
+Mousetrap.bind("j", function () {
+  cur = document.getElementsByClassName('item');
+  if (typeof list === 'undefined') { list = document.getElementsByClassName('item'); }
+  if (typeof i === 'undefined') { i = 0; }
+  if (i >= 0) {
+    if (i >= list.length) { list[i - 1].classList.remove('current'); i = 0; }
+    if (list[i - 1]) { list[i - 1].classList.remove('current'); };
+    if (list[i]) { list[i].classList.add('current'); }
+    i++;
+  }
+});
+Mousetrap.bind("k", function () {
+  if (typeof list === 'undefined') { list = document.getElementsByClassName('item'); }
+  if (typeof i === 'undefined') { i = list.length; }
+  if (i >= 0) {
+    i--;
+    if (i < 0) { list[i + 1].classList.remove('current'); i = list.length - 1; }
+    if (list[i + 1]) { list[i + 1].classList.remove('current'); };
+    if (list[i]) { list[i].classList.add('current'); }
+  }
+});
 //Mousetrap.bind('esc', function () { console.log('escape'); }, 'keyup');
 
-// combinations
+// key combinations
 Mousetrap.bind('command+shift+k', function () { alert('command shift k'); });
-Mousetrap.bind('#', function () { alert('# TODO: trigger delete for the active list item'); });
 
 // map multiple combinations to the same callback
-Mousetrap.bind(['command+k', 'ctrl+k'], function () {
-  console.log('command k or control k');
-
-  // return false to prevent default browser behavior
-  // and stop event from bubbling
-  return false;
-});
+// return false to prevent default browser behavior and stop event bubbling
+Mousetrap.bind(['n', '['], function () { document.getElementById('next').click(); return false; });
+Mousetrap.bind(['p', ']'], function () { document.getElementById('previous').click(); return false; });
 
 // gmail style sequences
-Mousetrap.bind('g i', function () { alert('go to inbox'); });
+Mousetrap.bind('g i', function () { document.getElementById('home').click(); });
 Mousetrap.bind('* a', function () { console.log('select all'); });
 
 // konami code!
 Mousetrap.bind('up up down down left right left right b a enter', function () {
-  console.log('konami code');
+  console.log('You did it!');
 });
