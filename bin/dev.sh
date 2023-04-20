c805f8aad0b01a743edadefee38ad45201b843f9
earthastronaut <an.email0101@gmail.com>
2023-04-20 09:26:45 -0700

    Make dev.sh script for dev work and the run.sh for prod

---
+++ bin/dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# set up the right python
+source ${ENVDIR}/bin/activate
+
+# TODO: wait for postgres at ${POSTGRES_HOST}:${POSTGRES_PORT}
+
+cd ${SOURCEDIR} && flask run --reload --host 0.0.0.0 --port 5000
--- bin/run.sh
+++ bin/run.sh
@@ -3,14 +3,10 @@
 # set up the right python
 source ${ENVDIR}/bin/activate
 
-# set up the environment variables
-# ~/www/tinybasilempire
-source ${SOURCEDIR}/.secrets
-
 # TODO: wait for postgres at ${POSTGRES_HOST}:${POSTGRES_PORT}
 
 # run your application with uvicorn
-gunicorn \
+cd ${SOURCEDIR} && gunicorn \
     --reload \
     --bind 0.0.0.0:5000 \
     --workers 4 \
