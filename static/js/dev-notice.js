87188d814b336c064782fc7683171b4c9cde1d62
Sheri Bigelow <sheri@designsimply.com>
2023-04-24 08:47:35 -0700

    Update dev notice color from tomato to mediumseagreen.

--- static/js/dev-notice.js
+++ static/js/dev-notice.js
@@ -1,5 +1,5 @@
 // give a visual indicator if we're not on the production server
 if (window.location.hostname != 'tinybasilempire.com') {
     var devnotice = document.head.appendChild(document.createElement("style"));
-    devnotice.innerHTML = "html { border-top: 1rem solid #ff303e; } html:before { content: '" + window.location.hostname + "'; color: white; background: #ff303e; font-size: 1rem; padding: 0 1em 0.25em 1em; right: 0; position: absolute; }";
+    devnotice.innerHTML = "html { border-top: 1rem solid mediumseagreen; } html:before { content: '" + window.location.hostname + "'; color: white; background: mediumseagreen; font-size: 1rem; padding: 0 1em 0.25em 1em; right: 0; position: absolute; }";
 }
